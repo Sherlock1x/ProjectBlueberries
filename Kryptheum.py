f8e86a9a8dee8d2cca3f9facdbf1a616d86151d9
Michae E. Giannuzzi <Sherlock1x@users.noreply.github.com>
2020-12-14 14:21:11 -0500

    Add files via upload

---
+++ Kryptheum.py
@@ -0,0 +1,293 @@
+
+
+
+#START: Pyramid_Chain_Container1 WvvBank_Master_Ledger 20201205 ATMWvvBank/ATM
+#Embed Code "Kryptheum PyramidChainContainer" Beta Virtual Machine"
+
+#Virtual Universe, Registered Byte Particle Element", a universe, registered byte particle element, 
+# that can never be directly detected, but whose existence does have measurable 
+# "Store of unit value"(souv) effects. Quote: "Stephen Hawking's"
+
+import datetime
+
+class WvvBank():
+    def __init__(self, Acctnum1):            
+        self.Acctnum1 = Acctnum1
+        #self.AcctBal1 = AcctBal1
+        #self.AcctRec1 = AcctRec1
+        #self.AcctPay1 = AcctPay1
+        ATM.counter = ATM.counter+1 
+
+class ATM(WvvBank):
+    def __init__(self, Acctnum1):               
+        super().__init__(Acctnum1)
+        #super().__init__(AcctBal1)             
+        #super().__init__(AcctRec1)
+        #super().__init__(AcctPay1)
+ 
+    @staticmethod
+    def find_Acctnum1(Acctnum1_money):
+        return Acctnum1_money * 05111948.1                  
+    #def find_AcctBal1(AcctBal1_money):
+        #return AcctBal1_money - 10000.1
+    # def find_AcctRec1(AcctRec1_money):
+    #     return AcctRec1_money  * 1000000.1
+    # def find_AcctPay1(AcctPay1_money):
+    #     return AcctPay1_money * 10000.1
+
+    counter = 0   
+    @classmethod
+    def object_count(cls):
+        return cls.counter  
+
+datetime_object = datetime.datetime.now()
+print(datetime_object)
+
+
+#newatm = 1000000.3
+x = ATM
+x = (x.object_count)
+#atm = ATM(1000000.3)
+atm = ATM(1.0)
+#atm = ATM(1000000.3)
+
+
+print('Acctnum1ByteParticles-WvvBankMasterLedger ->', 'worldvisionvirtualbank@mail.com', atm.find_Acctnum1(atm.Acctnum1))
+#print('AcctBal1BytePaticle->', atm.find_AcctBal1(atm.AcctBal1))
+#print('AcctRec1 ->', newatm.find_AcctRec1(newatm.AcctRec1))
+#print('AcctPay1 ->', newatm.find_AcctPay1(newatm.AcctPay1))
+#print(x)
+print(x())
+#print()
+#dir(atm)
+
+#>> NewMember = JuniorMember()
+# >>> NewMember.SetMemberName("Ali")
+# >>> NewMember.SetMemberID("12345")
+# >>> NewMember.MemberID
+# '12345'
+# >>> NewMember.MemberName
+# 'Ali'
+
+#Container1_AcctBal1_Master_Ledger
+class WvvBank():
+    def __init__(self, AcctBal1):            
+        #self.Acctnum1 = Acctnum1
+        self.AcctBal1 = AcctBal1
+        #self.AcctRec1 = AcctRec1
+        #self.AcctPay1 = AcctPay1
+        ATM.counter = ATM.counter+1
+
+class ATM(WvvBank):
+    def __init__(self, AcctBal1):               
+        #super().__init__(Acctnum1)
+        super().__init__(AcctBal1)             
+        #super().__init__(AcctRec1)
+        #super().__init__(AcctPay1)
+ 
+    @staticmethod
+    #def find_Acctnum1(Acctnum1_money):
+        #return Acctnum1_money * 123456.1                  
+    def find_AcctBal1(AcctBal1_money):
+        return AcctBal1_money - 10000.1
+    # def find_AcctRec1(AcctRec1_money):
+    #     return AcctRec1_money  * 1000000.1
+    # def find_AcctPay1(AcctPay1_money):
+    #     return AcctPay1_money * 10000.1
+
+    counter = 0   
+    @classmethod
+    def object_count(cls):
+        return cls.counter  
+
+x = ATM
+x = (x.object_count)
+#atm = ATM(1000000.3)
+#atm = ATM(1.0)
+atm = ATM(1000000.3)
+#newatm = ((1000000.3), (1.1), (1.1))
+
+#print('Acctnum1ByteParticles-WvvBankMasterLedger ->', atm.find_Acctnum1(atm.Acctnum1))
+print('AcctBal1BytePaticle->', atm.find_AcctBal1(atm.AcctBal1))
+#print('AcctRec1 ->', newatm.find_AcctRec1(newatm.AcctRec1))
+#print('AcctPay1 ->', newatm.find_AcctPay1(newatm.AcctPay1))
+#print(x)
+print(x())
+#print()
+
+
+#Container1_AcctRec1_Master_Ledger
+class WvvBank():
+    def __init__(self, AcctRec1):            
+        #self.Acctnum1 = Acctnum1
+        #self.AcctBal1 = AcctBal1
+        self.AcctRec1 = AcctRec1
+        #self.AcctPay1 = AcctPay1
+        ATM.counter = ATM.counter+1
+
+class ATM(WvvBank):
+    def __init__(self, AcctRec1):               
+        #super().__init__(Acctnum1)
+        #super().__init__(AcctBal1)             
+        super().__init__(AcctRec1)
+        #super().__init__(AcctPay1)
+ 
+    @staticmethod
+    #def find_Acctnum1(Acctnum1_money):
+        #return Acctnum1_money * 123456.1                  
+    #def find_AcctBal1(AcctBal1_money):
+        #return AcctBal1_money - 10000.1
+    def find_AcctRec1(AcctRec1_money):
+        return AcctRec1_money  * 1000000.1
+     #def find_AcctPay1(AcctPay1_money):
+         #return AcctPay1_money * 10000.1
+
+    counter = 0   
+    @classmethod
+    def object_count(cls):
+        return cls.counter  
+
+x = ATM
+x = (x.object_count)
+#atm = ATM(1000000.3)
+#atm = ATM(1.0)
+#atm = ATM(1000000.3)
+atm = ATM(1.0)
+
+#print('Acctnum1ByteParticles-WvvBankMasterLedger ->', atm.find_Acctnum1(atm.Acctnum1))
+#print('AcctBal1BytePaticle->', atm.find_AcctBal1(atm.AcctBal1))
+print('AcctRec1,ByteParticles->', atm.find_AcctRec1(atm.AcctRec1))
+#print('AcctPay1, ByteParticles ->', newatm.find_AcctPay1(newatm.AcctPay1))
+#print(x)
+print(x())
+#print()
+
+
+#Container1_AcctPay1_Master_Ledger
+class WvvBank():
+    def __init__(self, AcctPay1):            
+        #self.Acctnum1 = Acctnum1
+        #self.AcctBal1 = AcctBal1
+        #self.AcctRec1 = AcctRec1
+        self.AcctPay1 = AcctPay1
+        #self.AcctTrans1 = AcctTrans1
+        ATM.counter = ATM.counter+1
+
+class ATM(WvvBank):
+    def __init__(self, AcctPay1):              
+        #super().__init__(Acctnum1)
+        #super().__init__(AcctBal1)             
+        #super().__init__(AcctRec1)
+        super().__init__(AcctPay1)
+        #super().__init__(AcctTrans1)
+ 
+    @staticmethod
+    #def find_Acctnum1(Acctnum1_money):
+        #return Acctnum1_money * 123456.1                  
+    #def find_AcctBal1(AcctBal1_money):
+        #return AcctBal1_money - 10000.1
+    # def find_AcctRec1(AcctRec1_money):
+    #     return AcctRec1_money  * 1000000.1
+    def find_AcctPay1(AcctPay1_money):
+        return AcctPay1_money * 10000.1
+    #def  find_AcctTrans1(AcctTrans1_money):
+        #return AcctTrans1_money * 1.01
+
+    counter = 0   
+    @classmethod
+    def object_count(cls):
+        return cls.counter  
+
+x = ATM
+x = (x.object_count)
+#atm = ATM(1000000.3)
+#atm = ATM(1.0)
+atm = ATM(1.0)
+#atm = ATM(1.0)
+#atm = ATM(1000000.3)
+
+#print('Acctnum1ByteParticles-WvvBankMasterLedger ->', atm.find_Acctnum1(atm.Acctnum1))
+#print('AcctBal1BytePaticle->', atm.find_AcctBal1(atm.AcctBal1))
+#print('AcctRec1ByteParticles ->', newatm.find_AcctRec1(newatm.AcctRec1))
+print('AcctPay1ByteParticles ->', atm.find_AcctPay1(atm.AcctPay1))
+#print('AcctTrans1ByteParticles ->', atm.find_AcctTrans1(atm.AcctTrans1))
+#print(x)
+print(x())
+#print()
+
+
+
+#Container1_AcctTrans1_Master_Ledger
+class WvvBank():
+    def __init__(self, AcctTrans1):            
+        #self.Acctnum1 = Acctnum1
+        #self.AcctBal1 = AcctBal1
+        #self.AcctRec1 = AcctRec1
+        #self.AcctPay1 = AcctPay1
+        self.AcctTrans1 = AcctTrans1
+        ATM.counter = ATM.counter+1
+
+class ATM(WvvBank):
+    def __init__(self, AcctTrans1):              
+        #super().__init__(Acctnum1)
+        #super().__init__(AcctBal1)             
+        #super().__init__(AcctRec1)
+        #super().__init__(AcctPay1)
+        super().__init__(AcctTrans1)
+ 
+    @staticmethod
+    #def find_Acctnum1(Acctnum1_money):
+        #return Acctnum1_money * 123456.1                  
+    #def find_AcctBal1(AcctBal1_money):
+        #return AcctBal1_money - 10000.1
+    # def find_AcctRec1(AcctRec1_money):
+    #     return AcctRec1_money  * 1000000.1
+    #def find_AcctPay1(AcctPay1_money):
+        #return AcctPay1_money * 10000.1
+    def  find_AcctTrans1(AcctTrans1_money):
+        return AcctTrans1_money * 1.01
+
+    counter = 0   
+    @classmethod
+    def object_count(cls):
+        return cls.counter  
+
+x = ATM
+x = (x.object_count)
+#atm = ATM(1000000.3)
+#atm = ATM(1.0)
+#atm = ATM(1.0)
+atm = ATM(2.0)
+#atm = ATM(1000000.3)
+
+#print('Acctnum1ByteParticles-WvvBankMasterLedger ->', atm.find_Acctnum1(atm.Acctnum1))
+#print('AcctBal1BytePaticle->', atm.find_AcctBal1(atm.AcctBal1))
+#print('AcctRec1ByteParticles ->', newatm.find_AcctRec1(newatm.AcctRec1))
+#print('AcctPay1ByteParticles ->', atm.find_AcctPay1(atm.AcctPay1))
+print('AcctTrans1 ->', atm.find_AcctTrans1(atm.AcctTrans1))
+#print(x)
+print(x())
+#print()
+
+
+#END
+
+
+#https://docs.python.org/3/tutorial/datastructures.html?highlight=matrix%20arrays
+
+# matrix = [
+#       [10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000],
+#       [110000, 120000, 130000, 140000, 150000, 160000, 170000, 180000, 190000, 200000],
+#       [210000, 220000, 230000, 240000, 250000, 260000, 270000, 280000, 290000, 300000],
+#       [310000, 320000, 330000, 340000, 350000, 360000, 370000, 380000, 390000, 400000],
+#       [410000, 420000, 430000, 440000, 450000, 460000, 470000, 480000, 490000, 500000],
+#       [510000, 520000, 530000, 540000, 550000, 560000, 570000, 580000, 590000, 600000],
+#       [610000, 620000, 630000, 640000, 650000, 660000, 670000, 680000, 690000, 700000],
+#       [710000, 720000, 730000, 740000, 750000, 760000, 770000, 780000, 790000, 800000],
+#       [810000, 820000, 830000, 840000, 850000, 860000, 870000, 880000, 890000, 900000],
+#       [910000, 920000, 930000, 940000, 950000, 960000, 970000, 980000, 990000, 1000000],
+# ]
+
+# print(matrix, c, 'Container1/1000000 BytesParticles', 'WorldVisionVirtualBank@mail.com')
+
+
